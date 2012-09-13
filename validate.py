ae0c7070a25acc3c630086937c56c44de3ab8ac0
Donald Stufft <donald.stufft@gmail.com>
2012-09-12 20:53:23 -0400

    Add schema.py from halst and a validate.py that uses it

---
+++ validate.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+"""
+validate.py - Validates a distribution.json to verify it matches the requirement.
+"""
+import json
+import os
+import string
+import sys
+
+from schema import Schema, And, Use, Optional, SchemaError
+from distutils2 import version
+
+
+VALDIATOR = Schema({
+    "Metadata-Version": And(basestring, lambda x: x == "2.0"),
+    "Name": And(basestring, lambda x: "/" not in x),  # @@@ What exactly is "ok" for a name?
+    "Version": Use(version.NormalizedVersion),
+    "Summary": basestring,
+    Optional("Description"): And(basestring),  # @@@ Verify ReST?
+    Optional("Keywords"): [basestring],
+    Optional("Author"): basestring,
+    Optional("Author-Email"): And(basestring),  # @@@ Verify Proper Email
+    Optional("Maintainer"): basestring,
+    Optional("Maintainer-Email"): And(basestring),  # @@@ Verify Proper Email
+    Optional("License"): basestring,
+    Optional("Classifiers"): [basestring],
+    Optional("URIs"): {And(basestring, lambda x: len(x) <= 32): And(basestring)},  # @@@ Verify Valid URI
+    Optional("Platforms"): [basestring],
+    Optional("Supported-Platforms"): [basestring],
+    Optional("Provides-Extras"): [And(basestring, lambda x: not set(x) - (set(string.digits + string.ascii_letters + string.punctuation) - set("[],")))],
+    Optional("Setup-Requires-Dists"): [Use(version.VersionPredicate)],
+    Optional("Requires-Dists"): [Use(version.VersionPredicate)],
+    Optional("Provides-Dists"): [Use(version.VersionPredicate)],
+    Optional("Obsoletes-Dists"): [Use(version.VersionPredicate)],
+    Optional("Requires-Python"): basestring,  # @@@ Validate Version Specifier
+    Optional("Requires-Externals"): [basestring],
+    Optional("Extensions"): {basestring: {basestring: lambda x: True}},
+})
+
+
+if __name__ == "__main__":
+    paths = sys.argv[1:]
+
+    results = []
+
+    print ""
+
+    for path in paths:
+        data = None
+
+        with open(os.path.abspath(path)) as f:
+            data = json.load(f)
+
+        try:
+            VALDIATOR.validate(data)
+        except SchemaError as exc:
+            results.append(dict(file=path, status="FAILED"))
+
+            print path
+            print "=" * 50
+            print exc
+            print ""
+        else:
+            results.append(dict(file=path, status="OK"))
+
+    for result in results:
+        print "{file: <40}[{status}]".format(**result)
